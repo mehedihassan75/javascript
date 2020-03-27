2838508e85f3278750766041721bb7b6bea1b086
mehedihassan75 <mehedi.hassan.eu.181400075@gmail.com>
2020-03-28 01:04:21 +0600

    javascript

---
+++ datatypes.js
@@ -0,0 +1,16 @@
+var mehedi=10.10
+
+console.log(parseInt(mehedi))
+
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_SAFE_INTEGER)
+console.log(1/0)
+
+console.log(10/5)
+console.log('mehedi'*10)
+
+var abc=null
+var xyz
+
+console.log(abc)
+console.log(xyz)
