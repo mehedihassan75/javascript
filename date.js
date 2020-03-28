cf7b26540006e8cbebbb6f440d2198b0dfbf823e
mehedihassan75 <60974966+mehedihassan75@users.noreply.github.com>
2020-03-28 17:07:53 +0600

    Add files via upload

---
+++ date.js
@@ -0,0 +1,13 @@
+var mehedi=new Date()
+console.log(mehedi)
+console.log(mehedi.toDateString())
+console.log(mehedi.toTimeString())
+console.log(mehedi.toLocaleString())
+console.log(mehedi.getFullYear())
+console.log(mehedi.getMonth())
+console.log(mehedi.getDate())
+
+
+console.log(mehedi.getHours())
+console.log(mehedi.getMinutes())
+console.log(mehedi.getSeconds())
