3fa1bbc308fbf9498f3a1a0afe19e9dec88700a9
mehedihassan75 <60974966+mehedihassan75@users.noreply.github.com>
2020-03-29 00:10:37 +0600

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,26 @@
+var a=10
+var b=5
+console.log(a%b)
+console.log(++a)
+console.log(a++)
+console.log(a)
+
+console.log(b--)
+console.log(b)
+
+console.log(--b)
+
+
+var m=30
+var n=20
+console.log(m==n)
+console.log(m!=n)
+console.log(m>n)
+
+var c='10'
+var d=10
+console.log(c==d)
+console.log(c===d)
+console.log(c!==d)
+
+console.log(typeof'10')
