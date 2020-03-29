968458b283a13498bf8131d49af7040da034fae3
mehedihassan75 <60974966+mehedihassan75@users.noreply.github.com>
2020-03-29 12:29:59 +0600

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,23 @@
+var a=10
+var b=10
+var c=30
+
+if(a>b){
+    console.log(a+'is greater than '+b)
+}
+else if(b>a){
+    console.log(b+'is greater than '+a)
+}
+else{
+    console.log(c+' is greater')
+}
+
+
+var m=459
+if(m%2==0){
+    console.log(m+' is even number')
+}
+else{
+    console.log(m+' is odd number')
+}
+
