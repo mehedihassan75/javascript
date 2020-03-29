4886bab093e397ceb94e7ed987c714b0685b4dd4
mehedihassan75 <60974966+mehedihassan75@users.noreply.github.com>
2020-03-30 01:51:39 +0600

    Add files via upload

---
+++ project.js
@@ -0,0 +1,58 @@
+var date=new Date()
+
+var now=date.getMonth()
+
+switch(now){
+    case 0:
+        console.log('now is january')
+        break
+
+    case 1:
+        console.log('now is february')
+        break
+
+    case 2:
+        console.log('now is march')
+        break
+
+    case 3:
+        console.log('now is april')
+        break
+
+    case 4:
+        console.log('now is may')
+        break
+
+    case 5:
+        console.log('now is june')
+        break
+
+    case 6:
+        console.log('now is july')
+        break
+
+    case 7:
+        console.log('now is august')
+        break
+
+    case 8:
+        console.log('now is september')
+        break
+
+    case 9:
+        console.log('now is october')
+        break
+
+    case 10:
+        console.log('now is november')
+        break
+
+    case 11:
+        console.log('now is december')
+        break
+
+
+
+        default:
+            console.log('not a valid month')
+}
