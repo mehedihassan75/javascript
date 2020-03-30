d7808cb125d45bc603a23393aa6361bfef53765e
mehedihassan75 <60974966+mehedihassan75@users.noreply.github.com>
2020-03-30 15:48:53 +0600

    Add files via upload

---
+++ mathfunction.js
@@ -0,0 +1,19 @@
+console.log(Math.E)
+
+console.log(Math.PI)
+
+var m=3.1416
+console.log(Math.abs(m))
+console.log(Math.floor(m))
+console.log(Math.ceil(m))
+ 
+
+var n=10.49
+console.log(Math.round(n))
+
+console.log(Math.max(10,20))
+console.log(Math.min(10,20))
+
+console.log(Math.pow(2,6))
+
+console.log(Math.round(Math.random()*10+1))
