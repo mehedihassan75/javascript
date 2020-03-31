83f664f291823b0e8f72daeae2a2cb436eca63be
mehedihassan75 <60974966+mehedihassan75@users.noreply.github.com>
2020-03-31 21:34:42 +0600

    Add files via upload

---
+++ typeconversion.js
@@ -0,0 +1,15 @@
+var mehedi='1000'
+var reza=10
+
+console.log(Number(mehedi))
+console.log(reza.toString())
+console.log(Boolean(Infinity))
+console.log(Boolean(0))
+console.log(Boolean(null))
+
+console.log(Boolean(8586475888548))
+console.log(Boolean(-0))
+
+console.log(true)
+var mustakin=true
+console.log(mustakin.toString())
