d1eb8a74641dc150b9c3be3e55abfb1086c56035
mehedihassan75 <60974966+mehedihassan75@users.noreply.github.com>
2020-03-31 23:20:28 +0600

    Add files via upload

---
+++ nested loop.js
@@ -0,0 +1,18 @@
+var n=5
+for(var i=1;i<=n;i++){
+    var mehedi=''
+  for(var j=1;j<=i;j++){
+      mehedi+=j+ ' '
+  }
+  console.log(mehedi)
+}
+
+
+var n=7
+for(var i=1;i<=n;i++){
+    var reza=''
+    for(var j=1;j<=i;j++){
+        reza+= '* '
+    }
+    console.log(reza)
+}
