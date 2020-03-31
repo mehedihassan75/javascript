30103e31b0bce75b0897632f265fec115e4cd67d
mehedihassan75 <60974966+mehedihassan75@users.noreply.github.com>
2020-04-01 00:04:03 +0600

    Add files via upload

---
+++ break continue.js
@@ -0,0 +1,21 @@
+
+
+for(var i=1;i<10;i++){
+    if(i%9==0){
+        break
+    }
+    else{
+        console.log(i)
+    }
+}
+
+
+for(var mehedi=0;mehedi<10;mehedi++){
+    if(mehedi==0||mehedi==9){
+        continue
+    }
+    else{
+        console.log(mehedi)
+    }
+}
+
