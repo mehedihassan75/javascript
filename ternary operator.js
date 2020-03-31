b5f1d9badad0160a6534b82f3b4fd4ce48de9ca7
mehedihassan75 <60974966+mehedihassan75@users.noreply.github.com>
2020-04-01 00:07:04 +0600

    Add files via upload

---
+++ ternary operator.js
@@ -0,0 +1,16 @@
+var n=17
+var mehedi = n%2==0?'even':'odd'
+
+console.log(mehedi)
+
+
+
+
+var name='mustakin hassan'
+var fullname=name || 'mehedi hassan reza'
+console.log(fullname)
+
+
+
+var reza=true
+reza && console.log('everything is ok')
