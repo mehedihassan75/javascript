fba4ecb8bcef191fef91a6759287b1541e081192
mehedihassan75 <60974966+mehedihassan75@users.noreply.github.com>
2020-04-04 12:37:23 +0600

    Add files via upload

---
+++ string.js
@@ -0,0 +1,17 @@
+var n=10
+var str=String(n)
+console.log(n)
+
+
+var mehedi='bangladesh is a \'independent\' country'
+console.log(mehedi)
+
+
+//string comparison
+
+var mehedi='mbangladesh'
+var reza='india'
+console.log(mehedi>reza)
+
+
+console.log('002'==2)
