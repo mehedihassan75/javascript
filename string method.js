20282f3dd2fcef51cac33467cedf86ba714eaccf
mehedihassan75 <60974966+mehedihassan75@users.noreply.github.com>
2020-04-05 16:28:40 +0600

    Add files via upload

---
+++ string method.js
@@ -0,0 +1,24 @@
+var m='i am'
+var n='mehedi hassan reza'
+var o=m.concat(' ',n)
+var p=o.substr(5)
+
+
+console.log(p)
+
+//position of string
+console.log(o.charAt(10))
+
+ 
+//uppercase to lowercase
+
+console.log('MEHEDI'.toLowerCase())
+
+//lowercase to uppercase
+
+console.log('reza'.toUpperCase())
+
+
+console.log('   mustakin hassan munna   '.trim())
+ 
+console.log('mustakin hassan munna'.split(' '))
