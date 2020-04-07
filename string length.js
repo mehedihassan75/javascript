4466f67834abb187dd495b6fbc33794ea8e565a1
mehedihassan75 <60974966+mehedihassan75@users.noreply.github.com>
2020-04-07 12:28:07 +0600

    Add files via upload

---
+++ string length.js
@@ -0,0 +1,17 @@
+var str='shefali khatun'
+var length=0
+
+while(true){
+    if(str.charAt(length)==''){
+        break
+    }
+    else{
+        length++
+    }
+}
+
+console.log(length)
+
+//alternative way
+
+console.log(str.length)
